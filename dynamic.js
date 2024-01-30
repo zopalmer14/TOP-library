07e318ea5fe61c44278078367d29e88077006b3a
zopalmer14 <zopalmer14@gmail.com>
2024-01-30 16:57:26 -0500

    create frontend display for books in the library

--- dynamic.js
+++ dynamic.js
@@ -1,4 +1,13 @@
 
+// USEFUL DOM REFERENCES
+
+const grid_container = document.querySelector('#grid_container');
+const add_button = document.querySelector('#add_button');
+
+// LIBRARY FUNCTIONS
+
+const myLibrary = [];
+
 function Book(title, author, num_pages, have_read) {
     this.title = title;
     this.author = author;
@@ -8,3 +17,66 @@ function Book(title, author, num_pages, have_read) {
         return `${title} by ${author}, ${num_pages} pages, ${have_read ? 'already read' : 'not read yet'}`;
     }
 };
+
+function addBookToLibrary(title, author, num_pages, have_read) {
+    new_book = new Book(title, author, num_pages, have_read);
+    myLibrary.push(new_book);
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('The Way of Kings', 'Brandon Sanderson', 1093, true);
+addBookToLibrary('The Telling', 'Ursula K. Le Guin', 263, true);
+addBookToLibrary('Reaper Man', 'Terry Pratchett', 453, false);
+
+// CREATE/DISPLAY LIBRARY
+
+let book_container_style = `
+    display: flex;
+
+    background-color: lightgray;
+    margin: 2px 4px;
+    padding: 0px 2px;
+    flex: 1 1 0;
+`;
+
+let tile_style = `
+    background-color: white;
+    font-weight: bold;
+    margin: 4px 2px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1 1 0;
+`;
+
+function displayBooks() {
+    myLibrary.forEach((book) => {
+        // create a horizontal flex-container to hold the books info
+        const book_container = document.createElement('div');
+        book_container.style.cssText = book_container_style;
+        grid_container.appendChild(book_container);
+
+        // create tiles to display each property of the books
+        for (prop in book) {
+            let tile_info = '';
+            switch(prop) {
+                case 'info':
+                    continue;
+                case 'have_read':
+                    tile_info = book[prop] ? 'already read' : 'not read yet'
+                    break;
+                default:
+                    tile_info = book[prop];
+            }
+
+            // create a tile, assign the css styling, and add the property value
+            const tile = document.createElement('div');
+            tile.style.cssText = tile_style; 
+            tile.textContent = tile_info;
+            book_container.appendChild(tile);
+        }
+    });
+}
+
+displayBooks();
