4a7b8e84cf8cdfc209d654a70fa0b26df76338c3
zopalmer14 <zopalmer14@gmail.com>
2024-02-12 20:55:06 -0500

    implemented control flow and created links between DOM elements and back-end using the array index as the id

--- dynamic.js
+++ dynamic.js
@@ -14,41 +14,46 @@ const libraryManager = function libraryManager() {
         this.author = author;
         this.num_pages = num_pages;
         this.have_read = have_read;
-        this.info = function() {
-            return `${title} by ${author}, ${num_pages} pages, ${have_read ? 'already read' : 'not read yet'}`;
-        }
+        this.flipRead = () => this.have_read = (this.have_read) ? false : true; 
     }
 
     // function to add a new book to the library
-    const addBookToLibrary = function addBookToLibrary(title, author, num_pages, have_read) {
+    const addBook = function addBook(title, author, num_pages, have_read) {
         new_book = new Book(title, author, num_pages, have_read);
         myLibrary.push(new_book);
     };
 
-    return { getBooks, addBookToLibrary }
+    // function to remove a book from the library
+    const removeBook = function removeBook(index) {
+        myLibrary.splice(index, 1);
+    }
+
+    // function to toggle the read value of a book
+    const toggleRead = function toggleRead(index) {
+        myLibrary[index].flipRead();
+    }
+
+    return { getBooks, removeBook, addBook, toggleRead }
 }();
 
 // DOM MANIPULATION
 
 const DOMController = function DOMController() {
-    // function to handle the event of the user clicking the read status
-    function toggleRead(target) {
-        currentColor = target.style.backgroundColor;
-        currentText = target.textContent;
-    
-        // swap the color and text of the read? button when clicked
-        target.style.backgroundColor = currentColor === 'red' ? 'green' : 'red';
-        target.textContent = currentText === 'already read' ? 'have not read' : 'already read';
-    };
-
     const displayBooks = function displayBooks(books) {
-        // container that holds all the books
+        // grab the container that holds all the books
         const grid_container = document.querySelector('#grid-container');
 
-        books.forEach((book) => {
-            // create a horizontal flex-container to hold the books info
+        // remove all children in case of re-render
+        grid_container.replaceChildren();
+
+        // iterate over the books and create a visual representation of each
+        for (let i = 0; i < books.length; i++) {
+            const book = books[i];
+
+            // create a horizontal flex-container to hold the book's info
             const book_container = document.createElement('div');
             book_container.classList.add('book-container');
+            book_container.id = i;
             grid_container.appendChild(book_container);
     
             // create tiles to display each property of the books
@@ -57,7 +62,7 @@ const DOMController = function DOMController() {
                 let tile_info = '';
                 let tile_color = 'white';
                 switch(prop) {
-                    case 'info':
+                    case 'flipRead':
                         tile_info = 'Remove';
                         tile_color = 'lightgray';
                         break;
@@ -77,28 +82,53 @@ const DOMController = function DOMController() {
                 tile.textContent = tile_info;
                 book_container.appendChild(tile);
     
-                // add an eventListener to the read? button to toggle the value
+                // add an eventListener to the 'read' button to toggle the value
                 if (prop === 'have_read') {
-                    tile.addEventListener('click', (e) => {
-                        toggleRead(e.target);
-                    });
+                    setupReadButton(tile);
                 }
     
-                // add an eventListener to the remove? button to remove the book
-                if (prop === 'info') {
-                    tile.addEventListener('click', (e) => {
-                        // remove the book from the library
-                        e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-                    });
+                // add an eventListener to the 'remove' button to remove the book
+                if (prop === 'flipRead') {
+                    setupRemoveButton(tile);
                 } 
             }
-        });
+        }
     };
 
     return { displayBooks }
 }();
 
 // eventListeners & CONTROL FLOW 
+
+// function that makes the 'read' button dynamic / interactable
+function setupReadButton(button) {
+    button.addEventListener('click', (event) => {
+        // toggle the read value of the selected book
+        const index = event.target.parentNode.id;
+        libraryManager.toggleRead(index);
+
+        // also adjust the styling appropriately
+        // grab the current values
+        currentColor = event.target.style.backgroundColor;
+        currentText = event.target.textContent;
+    
+        // swap the color and text of the 'read' button
+        event.target.style.backgroundColor = currentColor === 'red' ? 'green' : 'red';
+        event.target.textContent = currentText === 'already read' ? 'have not read' : 'already read';
+    });
+}
+
+// function that makes the 'remove' button dynamic / interactable
+function setupRemoveButton(button) {
+    button.addEventListener('click', (event) => {
+        // remove the book from the library
+        const index = event.target.parentNode.id;
+        libraryManager.removeBook(index);
+        
+        // now we must re-render the library (reorder the indices - DOM to backend link)
+        DOMController.displayBooks(libraryManager.getBooks());
+    });
+}
 
 // useful DOM references
 const add_button = document.querySelector('#add-button');
@@ -113,14 +143,17 @@ add_button.addEventListener('click', () => {
 // setup the form processing / handling FORM PROCESSING / HANDLING
 add_book_form.addEventListener('submit', (event) => {
     // add the book to the library 
-    libraryManager.addBookToLibrary(event.target.title.value, event.target.author.value, event.target.pages.value, event.target.read.value);
+    libraryManager.addBook(event.target.title.value, event.target.author.value, event.target.pages.value, event.target.read.value);
+
+    // now we must re-render the library (reorder the indices - DOM to backend link)
+    DOMController.displayBooks(libraryManager.getBooks());
 });
 
 // debug / run script
 
-libraryManager.addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
-libraryManager.addBookToLibrary('The Way of Kings', 'Brandon Sanderson', 1093, true);
-libraryManager.addBookToLibrary('The Telling', 'Ursula K. Le Guin', 263, true);
-libraryManager.addBookToLibrary('Reaper Man', 'Terry Pratchett', 453, false);
+libraryManager.addBook('The Hobbit', 'J.R.R. Tolkien', 295, false);
+libraryManager.addBook('The Way of Kings', 'Brandon Sanderson', 1093, true);
+libraryManager.addBook('The Telling', 'Ursula K. Le Guin', 263, true);
+libraryManager.addBook('Reaper Man', 'Terry Pratchett', 453, false);
 
 DOMController.displayBooks(libraryManager.getBooks());
